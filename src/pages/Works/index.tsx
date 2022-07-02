80e639c3718f6a2ff5307964c0aaac763b0e727e
陳嘉旭 <73395592+BlaxBerry@users.noreply.github.com>
2022-07-02 15:10:18 +0900

    fix: modify Works children route component location

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { Outlet } from 'react-router-dom'
+import Toolbar from '@material-ui/core/Toolbar'
+import { Title, WrapTransparent, Tab } from '../../components/Common'
+import { TabItemsType } from '../../components/Common/Tab'
+import { worksChildrenRoute } from '../../components/Routes/RouterView'
 
 const IndexPage = (): JSX.Element => {
-  return (
-    <>
-      <div>
-        <Link to={'pc'}>PC</Link>
-        <Link to={'mobile'}>Mobile</Link>
-      </div>
+  const { t } = useTranslation()
 
+  const tabItems: TabItemsType[] = worksChildrenRoute.map((item) => ({
+    name: t(`pages.works.tabs.${item}`),
+    to: item,
+  }))
+
+  return (
+    <WrapTransparent>
+      {/* TODO: 顶间距占位 */}
+      <Toolbar variant="dense" />
+      <Toolbar variant="dense" />
+      <Toolbar variant="dense" />
+
+      {/* title */}
+      <Title title={t('pages.works.title')} />
+
+      {/* tab 选项 */}
+      <Tab fatherURL="works" tabItems={tabItems} />
+
+      {/* TODO: 间距 */}
+      <Toolbar variant="dense" />
+
+      {/* 子路由展示 */}
       <Outlet />
-    </>
+
+      {/* TODO: 间距 */}
+      <Toolbar variant="dense" />
+    </WrapTransparent>
   )
 }
 
